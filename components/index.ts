c33b00063bf7b8391a0ba05fb19bd3b3e71ec4dc
Juan Barros <juanlyrabarros@gmail.com>
2022-07-24 23:32:40 -0300

    Adiciona Client em domínio

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
 export { default as Layout } from "./Layout/Layout";
+export { default as List } from "./List/List";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { Layout } from "../components";
+import List from "../components/List/List";
 
 const IndexPage = () => (
   <Layout title="Home">
     <h1>BHub</h1>
+    <List items={[]} />
   </Layout>
 );
 
