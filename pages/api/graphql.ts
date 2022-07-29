e38373075c58a452afe7a59e6c249aac0ec3f6c3
Juan Barros <juanlyrabarros@gmail.com>
2022-07-29 14:42:06 -0300

    Adiciona modelo base de backend

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,18 +1,52 @@
 import { Cliente } from './../../domain/Cliente';
-import { createServer } from '@graphql-yoga/node'
+import { createServer, ResolverFn } from '@graphql-yoga/node'
 
 const typeDefs = /* GraphQL */ `
   type Query {
-    clientes: [Cliente]!
+    clientes: [Cliente]
   }
+
+  type Mutation {
+    createCliente(cliente: ClienteInput!): Cliente!
+  }
+
+  type DadoBancario {
+    agencia: String
+    conta: String
+    banco: String
+  }
+
+  type Cliente {
+    razaoSocial: String
+    telefone: String
+    endereco: String
+    dataCadastro: String
+    faturamentoDeclarado: Float
+    dadosBancarios: [DadoBancario]
+  }
+  input ClienteInput {
+    razaoSocial: String
+    telefone: String
+    endereco: String
+    dataCadastro: String
+    faturamentoDeclarado: Float
+  }
+
 `
+const clientes: Cliente[] = []
 
 const resolvers = {
     Query: {
         clientes() {
-            return [{ name: 'Nextjs' }]
+            return clientes
         },
     },
+    Mutation: {
+        createCliente(parent: unknown, args: { cliente: Cliente }) {
+            clientes.push(args.cliente)
+            return args.cliente
+        }
+    }
 }
 
 const server = createServer({
